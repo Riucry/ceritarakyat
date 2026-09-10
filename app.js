74db648d2e3b500d4dec3f2c6fe11b7472ca9dca
Riucry <riu606242@gmail.com>
2026-09-10 18:40:20 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,579 @@
+/* =====================================================
+   DATA CERITA
+===================================================== */
+
+const stories = [
+
+    {
+        title: "Asal Usul Danau Toba",
+
+        image: "img/images/asal usul danau toba.jpg",
+
+        description:
+            "Pada zaman dahulu, hiduplah seorang petani bernama Toba. Ia tinggal seorang diri di sebuah daerah yang subur. Setiap hari Toba bekerja dengan tekun untuk memenuhi kebutuhan hidupnya. Suatu hari, ketika sedang memancing di sungai, Toba mendapatkan seekor ikan yang sangat indah. Ternyata ikan tersebut dapat berubah menjadi seorang perempuan. Perempuan itu kemudian menjadi istri Toba dengan satu syarat, yaitu Toba tidak boleh menceritakan asal-usulnya kepada siapa pun. Mereka kemudian memiliki seorang anak bernama Samosir. Namun suatu hari Toba sangat marah kepada Samosir dan tanpa sengaja mengatakan bahwa Samosir adalah anak ikan. Istri Toba sangat sedih karena Toba telah melanggar janjinya. Kemudian turunlah hujan yang sangat deras dan air memenuhi daerah tersebut hingga terbentuklah sebuah danau yang sangat luas. Danau itu kemudian dikenal sebagai Danau Toba dan pulau di tengahnya dikenal sebagai Pulau Samosir.",
+
+        moral:
+            "Kita harus menjaga janji, mengendalikan emosi, dan tidak mengucapkan perkataan yang dapat menyakiti orang lain."
+    },
+
+
+    {
+        title: "Malin Kundang",
+
+        image: "img/images/malin kundang.jpg",
+
+        description:
+            "Pada zaman dahulu, hiduplah seorang anak bernama Malin Kundang bersama ibunya. Mereka hidup sederhana dan serba kekurangan. Setelah dewasa, Malin memutuskan untuk merantau dan mencari kehidupan yang lebih baik. Ia kemudian menjadi seorang yang kaya raya dan menikah dengan perempuan dari keluarga berada. Suatu hari Malin kembali ke kampung halamannya dengan kapal yang besar. Ibunya yang mengetahui kedatangan Malin segera datang menyambut anaknya. Namun Malin merasa malu mengakui ibunya karena penampilannya yang sederhana. Ia bahkan mengatakan bahwa perempuan tua tersebut bukan ibunya. Sang ibu merasa sangat sedih dan kecewa. Ia kemudian berdoa agar Malin mendapatkan pelajaran atas perbuatannya. Tidak lama kemudian badai datang dan kapal Malin hancur. Malin pun berubah menjadi batu.",
+
+        moral:
+            "Kita harus selalu menghormati dan menyayangi orang tua serta tidak boleh menjadi sombong ketika sudah berhasil."
+    },
+
+
+    {
+        title: "Si Kancil dan Buaya",
+
+        image: "img/images/kancil dan buaya.jpg",
+
+        description:
+            "Pada suatu hari, Si Kancil ingin menyeberangi sungai untuk mencari makanan. Namun sungai tersebut dipenuhi oleh banyak buaya. Kancil kemudian menggunakan kecerdikannya. Ia mengatakan kepada para buaya bahwa dirinya diperintahkan oleh raja untuk menghitung jumlah buaya di sungai. Para buaya kemudian berbaris dari satu sisi sungai hingga sisi lainnya. Kancil melompat dari satu buaya ke buaya lainnya sambil berpura-pura menghitung. Setelah sampai di seberang sungai, Kancil tertawa karena berhasil menipu para buaya dan melanjutkan perjalanannya.",
+
+        moral:
+            "Kecerdikan dapat membantu menyelesaikan masalah, tetapi kita tetap harus menggunakan kecerdikan dengan bijaksana dan tidak merugikan orang lain."
+    },
+
+
+    {
+        title: "Keong Mas",
+
+        image: "img/images/keong mas.jpg",
+
+        description:
+            "Dahulu kala, terdapat seorang putri cantik bernama Candra Kirana. Ia memiliki seorang saudari bernama Dewi Galuh yang merasa iri kepadanya. Karena rasa iri tersebut, Dewi Galuh meminta bantuan seorang penyihir untuk mengutuk Candra Kirana menjadi seekor keong emas. Keong tersebut kemudian ditemukan oleh seorang nenek yang baik hati. Setiap kali nenek pergi, keong emas berubah menjadi seorang perempuan cantik dan membantu pekerjaan rumah. Suatu hari, sang pangeran mengetahui bahwa Candra Kirana berada di rumah nenek tersebut. Dengan bantuan pangeran, kutukan akhirnya dapat dihilangkan. Candra Kirana kembali menjadi manusia dan kemudian hidup bahagia.",
+
+        moral:
+            "Jangan iri terhadap keberhasilan orang lain. Kebaikan, kesabaran, dan ketulusan pada akhirnya akan membawa kebahagiaan."
+    },
+
+
+    {
+        title: "Batu Menangis",
+
+        image: "img/images/Batu menangis.jpg",
+
+        description:
+            "Di sebuah desa, hiduplah seorang ibu bersama anak perempuannya. Anak tersebut sangat cantik tetapi memiliki sifat sombong dan tidak mau mengakui ibunya yang miskin. Suatu hari mereka pergi ke pasar. Dalam perjalanan, banyak orang yang memuji kecantikan anak tersebut. Ketika seseorang bertanya siapa perempuan yang berjalan bersamanya, sang anak dengan malu mengatakan bahwa perempuan itu adalah pembantunya. Sang ibu merasa sangat sedih mendengar perkataan anaknya. Ia kemudian berdoa kepada Tuhan agar anaknya diberi pelajaran. Tidak lama kemudian, tubuh anak tersebut perlahan berubah menjadi batu. Ia menangis dan menyesal, tetapi semuanya sudah terlambat.",
+
+        moral:
+            "Jangan pernah malu terhadap orang tua. Kita harus menghormati, menyayangi, dan menghargai pengorbanan mereka."
+    },
+
+
+    {
+        title: "Sangkuriang",
+
+        image: "img/images/Sangkuriang.jpg",
+
+        description:
+            "Pada zaman dahulu, hiduplah seorang perempuan bernama Dayang Sumbi. Ia memiliki seorang anak bernama Sangkuriang. Karena suatu kejadian, Sangkuriang pergi meninggalkan ibunya. Bertahun-tahun kemudian, Sangkuriang kembali tanpa mengetahui bahwa perempuan yang ditemuinya adalah ibunya sendiri. Ia kemudian jatuh cinta kepada Dayang Sumbi dan ingin menikahinya. Dayang Sumbi akhirnya mengetahui bahwa Sangkuriang adalah anaknya. Untuk menggagalkan pernikahan tersebut, Dayang Sumbi memberikan syarat agar Sangkuriang membuat sebuah danau dan perahu dalam waktu satu malam. Sangkuriang hampir berhasil. Namun Dayang Sumbi membuat suasana seolah-olah pagi telah tiba. Sangkuriang marah dan menendang perahu yang dibuatnya hingga terbalik. Perahu tersebut kemudian dipercaya menjadi Gunung Tangkuban Perahu.",
+
+        moral:
+            "Kita harus menghormati orang tua, berpikir dengan tenang, dan tidak mudah dikuasai oleh amarah."
+    },
+
+
+    {
+        title: "Roro Jonggrang",
+
+        image: "img/images/roro jonggrang.jpg",
+
+        description:
+            "Roro Jonggrang adalah seorang putri cantik dari Kerajaan Prambanan. Suatu hari kerajaan tersebut dikalahkan oleh Bandung Bondowoso. Bandung Bondowoso kemudian jatuh cinta kepada Roro Jonggrang dan ingin menikahinya. Roro Jonggrang tidak ingin menikah dengannya. Ia kemudian memberikan syarat yang sangat sulit, yaitu Bandung Bondowoso harus membuat seribu candi dalam satu malam. Dengan bantuan makhluk gaib, Bandung Bondowoso hampir menyelesaikan seribu candi tersebut. Roro Jonggrang kemudian memerintahkan para perempuan untuk menyalakan api dan membuat suara seperti pagi telah tiba. Para makhluk gaib mengira pagi sudah datang dan berhenti bekerja. Bandung Bondowoso mengetahui bahwa dirinya telah ditipu. Ia kemudian mengutuk Roro Jonggrang menjadi sebuah arca untuk melengkapi candi yang terakhir.",
+
+        moral:
+            "Kita harus berhati-hati dalam membuat keputusan dan tidak menggunakan tipu daya untuk menyelesaikan masalah."
+    },
+
+
+    {
+        title: "Timun Mas",
+
+        image: "img/images/Timun mas.jpg",
+
+        description:
+            "Pada zaman dahulu, hiduplah seorang janda bernama Mbok Srini. Ia sangat ingin memiliki seorang anak. Suatu hari ia bertemu dengan raksasa yang memberikan biji mentimun kepadanya. Dari salah satu mentimun tersebut lahirlah seorang bayi perempuan yang diberi nama Timun Mas. Raksasa tersebut mengatakan bahwa ketika Timun Mas sudah dewasa, ia harus menyerahkan Timun Mas kepadanya. Mbok Srini sangat menyayangi Timun Mas. Ketika Timun Mas mulai dewasa, raksasa datang untuk mengambilnya. Timun Mas melarikan diri dengan membawa beberapa benda pemberian seorang pertapa. Ia melemparkan biji mentimun, jarum, garam, dan terasi. Semua benda tersebut berubah menjadi rintangan yang menyulitkan raksasa. Akhirnya Timun Mas berhasil menyelamatkan diri.",
+
+        moral:
+            "Keberanian, usaha, dan doa dapat membantu kita menghadapi kesulitan. Jangan mudah menyerah ketika menghadapi masalah."
+    },
+
+
+    {
+        title: "Telaga Warna",
+
+        image: "img/images/Telaga Warna.jpg",
+
+        description:
+            "Pada zaman dahulu, terdapat sebuah kerajaan yang dipimpin oleh seorang raja dan ratu. Mereka memiliki seorang putri yang sangat cantik. Namun sang putri memiliki sifat manja dan sering meminta berbagai hadiah. Ketika ulang tahunnya tiba, seluruh rakyat memberikan sebuah kalung yang sangat indah. Namun sang putri tidak menyukai hadiah tersebut dan melemparkannya hingga pecah. Rakyat merasa sangat sedih. Tiba-tiba dari tempat kalung itu jatuh muncul air yang sangat banyak. Air tersebut memenuhi wilayah kerajaan dan membentuk sebuah telaga. Air telaga itu memiliki berbagai macam warna yang indah. Tempat tersebut kemudian dikenal sebagai Telaga Warna.",
+
+        moral:
+            "Kita harus bersyukur atas pemberian orang lain dan tidak menjadi anak yang sombong atau terlalu manja."
+    },
+
+
+    {
+        title: "Lutung Kasarung",
+
+        image: "img/images/Lutung Kasarung.jpg",
+
+        description:
+            "Di sebuah kerajaan, terdapat seorang putri bernama Purbasari yang memiliki hati baik. Namun saudarinya, Purbararang, merasa iri kepada Purbasari. Ia kemudian melakukan berbagai cara untuk menyingkirkan Purbasari dari kerajaan. Purbasari akhirnya tinggal di hutan. Di sana ia bertemu dengan seekor lutung yang sebenarnya adalah seorang pangeran yang terkena kutukan. Lutung Kasarung selalu membantu Purbasari dan melindunginya. Dengan bantuan Lutung Kasarung, Purbasari berhasil melewati berbagai ujian. Pada akhirnya kebenaran terungkap dan Lutung Kasarung kembali menjadi seorang pangeran. Purbasari kemudian mendapatkan kembali kedudukannya dan hidup bahagia.",
+
+        moral:
+            "Kebaikan hati, kesabaran, dan ketulusan akan membawa kebaikan. Jangan iri terhadap keberhasilan orang lain."
+    }
+
+];
+
+
+/* =====================================================
+   VARIABEL
+===================================================== */
+
+let currentStory = 0;
+
+let speech = null;
+
+let musicStarted = false;
+
+
+/* =====================================================
+   ELEMENT HTML
+===================================================== */
+
+const storyImage = document.getElementById("storyImage");
+
+const storyTitle = document.getElementById("storyTitle");
+
+const storyDescription = document.getElementById("storyDescription");
+
+const moralText = document.getElementById("moralText");
+
+const pageCounter = document.getElementById("pageCounter");
+
+const dotsContainer = document.getElementById("dots");
+
+const backgroundMusic = document.getElementById("backgroundMusic");
+
+const musicButton = document.getElementById("musicButton");
+
+
+/* =====================================================
+   PENGATURAN MUSIK
+===================================================== */
+
+backgroundMusic.volume = 0.20;
+
+
+/* =====================================================
+   TAMPILKAN CERITA
+===================================================== */
+
+function showStory() {
+
+    const story = stories[currentStory];
+
+
+    storyImage.src = story.image;
+
+    storyImage.alt = story.title;
+
+
+    storyTitle.textContent = story.title;
+
+
+    storyDescription.textContent = story.description;
+
+
+    moralText.textContent = story.moral;
+
+
+    pageCounter.textContent =
+        `Halaman ${currentStory + 1} dari ${stories.length}`;
+
+
+    createDots();
+}
+
+
+/* =====================================================
+   MEMBUAT DOT
+===================================================== */
+
+function createDots() {
+
+    dotsContainer.innerHTML = "";
+
+
+    stories.forEach((story, index) => {
+
+        const dot = document.createElement("span");
+
+        dot.classList.add("dot");
+
+
+        if (index === currentStory) {
+            dot.classList.add("active");
+        }
+
+
+        dot.title = story.title;
+
+
+        dot.onclick = function () {
+
+            stopStory();
+
+            currentStory = index;
+
+            showStory();
+
+        };
+
+
+        dotsContainer.appendChild(dot);
+
+    });
+
+}
+
+
+/* =====================================================
+   MENCARI SUARA INDONESIA
+===================================================== */
+
+function getIndonesianVoice() {
+
+    const voices = speechSynthesis.getVoices();
+
+
+    let voice = voices.find(
+        v => v.lang === "id-ID"
+    );
+
+
+    if (!voice) {
+
+        voice = voices.find(
+            v => v.lang.toLowerCase().startsWith("id")
+        );
+
+    }
+
+
+    return voice;
+}
+
+
+/* =====================================================
+   PUTAR CERITA
+===================================================== */
+
+function speakStory() {
+
+    if (!("speechSynthesis" in window)) {
+
+        alert(
+            "Browser kamu tidak mendukung fitur suara."
+        );
+
+        return;
+
+    }
+
+
+    speechSynthesis.cancel();
+
+
+    const story = stories[currentStory];
+
+
+    const text =
+        `${story.title}. ` +
+        `${story.description}. ` +
+        `Pesan moral. ${story.moral}`;
+
+
+    speech = new SpeechSynthesisUtterance(text);
+
+
+    speech.lang = "id-ID";
+
+    speech.rate = 0.85;
+
+    speech.pitch = 1;
+
+    speech.volume = 1;
+
+
+    const voice = getIndonesianVoice();
+
+
+    if (voice) {
+        speech.voice = voice;
+    }
+
+
+    speechSynthesis.speak(speech);
+
+}
+
+
+/* =====================================================
+   JEDA SUARA
+===================================================== */
+
+function pauseStory() {
+
+    if ("speechSynthesis" in window) {
+
+        speechSynthesis.pause();
+
+    }
+
+}
+
+
+/* =====================================================
+   LANJUTKAN SUARA
+===================================================== */
+
+function resumeStory() {
+
+    if ("speechSynthesis" in window) {
+
+        speechSynthesis.resume();
+
+    }
+
+}
+
+
+/* =====================================================
+   BERHENTI SUARA
+===================================================== */
+
+function stopStory() {
+
+    if ("speechSynthesis" in window) {
+
+        speechSynthesis.cancel();
+
+    }
+
+}
+
+
+/* =====================================================
+   CERITA BERIKUTNYA
+===================================================== */
+
+function nextStory() {
+
+    stopStory();
+
+
+    if (currentStory < stories.length - 1) {
+
+        currentStory++;
+
+    } else {
+
+        currentStory = 0;
+
+    }
+
+
+    showStory();
+
+
+    setTimeout(() => {
+
+        speakStory();
+
+    }, 500);
+
+}
+
+
+/* =====================================================
+   CERITA SEBELUMNYA
+===================================================== */
+
+function previousStory() {
+
+    stopStory();
+
+
+    if (currentStory > 0) {
+
+        currentStory--;
+
+    } else {
+
+        currentStory = stories.length - 1;
+
+    }
+
+
+    showStory();
+
+
+    setTimeout(() => {
+
+        speakStory();
+
+    }, 500);
+
+}
+
+
+/* =====================================================
+   KEMBALI KE CERITA PERTAMA
+===================================================== */
+
+function backToStart() {
+
+    stopStory();
+
+
+    currentStory = 0;
+
+
+    showStory();
+
+
+    setTimeout(() => {
+
+        speakStory();
+
+    }, 500);
+
+}
+
+
+/* =====================================================
+   MUSIK BACKGROUND
+===================================================== */
+
+function toggleMusic() {
+
+    if (backgroundMusic.paused) {
+
+        backgroundMusic.play()
+
+            .then(() => {
+
+                musicStarted = true;
+
+                musicButton.textContent =
+                    "🎵 Musik ON";
+
+                musicButton.classList.add("active");
+
+            })
+
+            .catch((error) => {
+
+                console.log(
+                    "Musik gagal diputar:",
+                    error
+                );
+
+
+                alert(
+                    "Musik belum bisa diputar. Pastikan file musik-dongeng.mp3 ada di folder audio."
+                );
+
+            });
+
+    } else {
+
+        backgroundMusic.pause();
+
+
+        musicButton.textContent =
+            "🎵 Musik OFF";
+
+
+        musicButton.classList.remove("active");
+
+    }
+
+}
+
+
+/* =====================================================
+   OTOMATIS MENCOBA MEMULAI MUSIK
+===================================================== */
+
+document.addEventListener(
+    "click",
+    function startMusicAfterClick() {
+
+        if (
+            !musicStarted &&
+            backgroundMusic.paused
+        ) {
+
+            backgroundMusic.play()
+
+                .then(() => {
+
+                    musicStarted = true;
+
+                    musicButton.textContent =
+                        "🎵 Musik ON";
+
+                    musicButton.classList.add(
+                        "active"
+                    );
+
+                })
+
+                .catch(() => {
+
+                    // Browser menolak autoplay.
+                    // User tinggal klik tombol Musik.
+
+                });
+
+        }
+
+    },
+    {
+        once: true
+    }
+);
+
+
+/* =====================================================
+   LOAD SUARA BROWSER
+===================================================== */
+
+if ("speechSynthesis" in window) {
+
+    speechSynthesis.onvoiceschanged = function () {
+
+        getIndonesianVoice();
+
+    };
+
+}
+
+
+/* =====================================================
+   JALANKAN WEBSITE
+===================================================== */
+
+showStory();
